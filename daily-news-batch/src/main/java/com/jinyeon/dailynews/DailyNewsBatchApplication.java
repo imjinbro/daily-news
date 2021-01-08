f882a4ab71af68bde17dadf0b076fe0164122cc9
박진형 <jinbro@woowahan.com>
2021-01-08 12:40:29 +0900

    프로젝트 초기 설정

---
+++ daily-news-batch/src/main/java/com/jinyeon/dailynews/DailyNewsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.jinyeon.dailynews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyNewsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyNewsBatchApplication.class, args);
+	}
+
+}
---
+++ daily-news-batch/src/test/java/com/jinyeon/dailynews/DailyNewsBatchApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jinyeon.dailynews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyNewsBatchApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
